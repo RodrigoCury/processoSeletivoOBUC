da24b9f7e247f0b7546eca0d95a7236182fd8569
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-31 14:47:40 -0300

    manipulador de ícones

---
+++ src/javascript/icons/index.js
@@ -0,0 +1,43 @@
+const feather = require('feather-icons')
+
+class Icons {
+    constructor() {
+        this.setupIcons()
+    }
+
+    setupIcons() {
+        this.homeIcon = feather.icons.home.toSvg({
+            class: "home-feather",
+            color: "#FFF",
+            'stroke-width': 1.5,
+        })
+
+        this.userIcon = feather.icons.user.toSvg({
+            class: "user-feather",
+            color: "#FFF",
+            'stroke-width': 1.5,
+        })
+
+        this.gearIcon = feather.icons.settings.toSvg({
+            class: "user-feather",
+            color: "#FFF",
+            'stroke-width': 1.5,
+            height: '1em',
+            width: '1em'
+        })
+
+        this.editIcon = feather.icons['edit-3'].toSvg({
+            class: "edit-feather",
+            color: "#001b2b",
+            'stroke-width': 1.5,
+        })
+
+        this.trashIcon = feather.icons['trash-2'].toSvg({
+            class: "trash-feather",
+            color: "#001b2b",
+            'stroke-width': 1.5,
+        })
+    }
+}
+
+module.exports = Icons
