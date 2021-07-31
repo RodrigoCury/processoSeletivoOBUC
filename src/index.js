3ee3e2e5e4196196162eb0af6c58fb0436766844
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-31 14:46:10 -0300

    Chama os estilos da página

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style/main.css'
-// import './style/media-queries.css'
+import './style/header.css'
+import './style/nav.css'
+import './style/workPlaceTask.css'
 const Application = require('./javascript/Application.js')
 
 window.application = new Application()
